d0158d135965864906bfe968029737b232723819
Pesi <arthur.pesi@gmail.com>
2025-06-06 17:34:53 -0300

    voronoi cam v 1.0, just lacking a bit of adjustments

--- levels/camera_1.gd
+++ levels/camera_1.gd
@@ -2,9 +2,11 @@ extends Camera2D
 
 @onready var world = $"../../../.."
 var player_1
+var player_2
 
 func _ready() -> void:
 	player_1 = world.players[0]
+	player_2 = world.players[1]
 	
 func _process(delta: float) -> void:
-	position = player_1.position
+	position = lerp(player_1.position, player_2.position, -0.3)
--- levels/camera_2.gd
+++ levels/camera_2.gd
@@ -2,9 +2,11 @@ extends Camera2D
 
 @onready var world = $"../../../.."
 var player_2
+var player_1
 
 func _ready() -> void:
+	player_1 = world.players[0]
 	player_2 = world.players[1]
 	
 func _process(delta: float) -> void:
-	position = player_2.position
+	position = lerp(player_2.position, player_1.position, -0.3)
